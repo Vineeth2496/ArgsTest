35518c6cb5acc23690f8968d8c7ab0ae3b548405
Vineeth2496 <vineetharmoori2496@gmail.com>
2024-03-08 03:09:20 +0530

    Initial commit

---
+++ src/com/vie/Test.java
@@ -0,0 +1,17 @@
+package com.vie;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hai");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[3]);
+		System.out.println("=========");
+		// DdbExport1=somedata
+		System.out.println(System.getProperty("dbExport1"));
+	}
+
+}
